7352eb0ffdab992c624fa2b15b844d0fa1382d4b
Atlinx <atlinuxtm@gmail.com>
2026-05-21 15:53:10 -0400

    Fix run_container for newer docker installs w/o nvidia runtime (uses nvidia-container-toolkit)

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,8 +1,8 @@
-docker rm -f ffs || true
-xhost +local:root || true
+docker rm -f ffs >/dev/null 2>&1 || true
+xhost +local:root >/dev/null 2>&1 || true
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 REPO_DIR=$SCRIPT_DIR/../
-docker run --gpus all --runtime nvidia --env NVIDIA_DISABLE_REQUIRE=1 -it \
+docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it \
     --network=host --name ffs --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
     -v $REPO_DIR:/workspace --ipc=host -e DISPLAY=${DISPLAY} \
     -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp -v /home:/home -v /mnt:/mnt -w /workspace ffs \
